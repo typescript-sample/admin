c417c908a34fc361916079774a0665488f9e6b73
thanhhaile051020 <thanhhaile0510@gmail.com>
2022-04-01 18:38:38 +0700

    update app.ts multiple domains

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import { merge } from 'config-plus';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
-import express, { json } from 'express';
-import { allow, loadTemplates, MiddlewareLogger, start } from 'express-ext';
+import express, { json ,NextFunction} from 'express';
+import { loadTemplates, MiddlewareLogger, start } from 'express-ext';
 import { createLogger } from 'logger-core';
 import { createPool } from 'mysql';
 import { PoolManager } from 'mysql-core';
@@ -11,6 +11,26 @@ import { buildTemplates, trim } from 'query-mappers';
 import { config, env } from './config';
 import { useContext } from './context';
 import { route } from './route';
+import { Request, Response } from 'express';
+export interface AccessConfig {
+  origin: string | string[];
+  credentials: string | string[];
+  methods: string | string[];
+  headers: number | string | ReadonlyArray<string>;
+}
+export type AccessControlAllowConfig = AccessConfig;
+export function allow(access: AccessConfig): (req: Request, res: Response, next: NextFunction) => void {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const origin = req.headers.origin || 'http://localhost:3000';
+    if (access.origin.includes(origin)) {
+      res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.header('Access-Control-Allow-Credentials', access.credentials);
+    res.header('Access-Control-Allow-Methods', access.methods);
+    res.setHeader('Access-Control-Allow-Headers', access.headers);
+    next();
+  };
+}
 
 dotenv.config();
 const conf = merge(config, process.env, env, process.env.ENV);
